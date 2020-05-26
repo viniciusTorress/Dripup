f9e0832af774a475c8454e320c9f6c31fc2c1afe
vinialvestorres@gmail.com <vinialvestorres@gmail.com>
2020-05-26 18:39:34 -0300

    segundo commit

--- src/pages/Acessorios/index.js
+++ src/pages/Acessorios/index.js
@@ -25,26 +25,24 @@ function Acessorios () {
     return(
         <>
             <Header/>
-
-
             <ConteinerAcessorios> 
                 <DivAcessorios>      
                 <nav>
                     <ul>
                         <li>
                             
-                                <img src={carteira} />
-                                <h5>R$ 2400.000,00</h5>
-                                <p>Gucci Carteira Couro de Carneiro</p>
-                                <Link to="/Comprar"><button>Comprar</button></Link>
+                            <img src={carteira} />
+                            <h5>R$ 2400.000,00</h5>
+                            <p>Gucci Carteira Couro de Carneiro</p>
+                            <Link to="/Comprar"><button>Comprar</button></Link>
                             
                         </li>
                         <li>
                             
-                                <img src={cinto} />
-                                <h5>R$ 1.391,00</h5>
-                                <p>Off-White Cinto Logo Industrial Feminino</p>
-                                <Link to="/Comprar"><button>Comprar</button></Link>
+                            <img src={cinto} />
+                            <h5>R$ 1.391,00</h5>
+                            <p>Off-White Cinto Logo Industrial Feminino</p>
+                            <Link to="/Comprar"><button>Comprar</button></Link>
                             
                         </li>
                         <li>
@@ -57,75 +55,75 @@ function Acessorios () {
                         </li>
                         <li>
                             
-                                <img src={hardy} />
-                                <h5>R$ 23.219,00.</h5>
-                                <p>John Hardy Pulseira Modern Chain de Prata com Ouro 18K</p>
-                                <Link to="/Comprar"><button>Comprar</button></Link>
+                            <img src={hardy} />
+                            <h5>R$ 23.219,00.</h5>
+                            <p>John Hardy Pulseira Modern Chain de Prata com Ouro 18K</p>
+                            <Link to="/Comprar"><button>Comprar</button></Link>
                             
                         </li>
                     
                         <li>
                             
-                                <img src={chapeu} />
-                                <h5>R$ 21.140,00</h5>
-                                <p>Takahiromiyashita the Soloist Chapéu Nick Fouquet com Pluma</p>
-                                <Link to="/Comprar"><button>Comprar</button></Link>
+                            <img src={chapeu} />
+                            <h5>R$ 21.140,00</h5>
+                            <p>Takahiromiyashita the Soloist Chapéu Nick Fouquet com Pluma</p>
+                            <Link to="/Comprar"><button>Comprar</button></Link>
                             
                         </li>
                         <li>
                             
-                                <img src={colar} />
-                                <h5>R$ 20.397,00</h5>
-                                <p>Bottega Veneta Colar com Corrente Esculpida</p>
-                                <Link to="/Comprar"><button>Comprar</button></Link>
+                            <img src={colar} />
+                            <h5>R$ 20.397,00</h5>
+                            <p>Bottega Veneta Colar com Corrente Esculpida</p>
+                            <Link to="/Comprar"><button>Comprar</button></Link>
                             
                         </li>
                         <li>
                             
-                                <img src={tom} />
-                                <h5>R$ 20.128,00</h5>
-                                <p>Tom Wood Anel Moeda e Seda </p>
-                                <Link to="/Comprar"><button>Comprar</button></Link>
+                            <img src={tom} />
+                            <h5>R$ 20.128,00</h5>
+                            <p>Tom Wood Anel Moeda e Seda </p>
+                            <Link to="/Comprar"><button>Comprar</button></Link>
                             
                         </li>
                         <li>
                             
-                                <img src={corrente} />
-                                <h5>R$ 5.400,00</h5>
-                                <p>Prada Colar com Corrente</p>
-                                <Link to="/Comprar"><button>Comprar</button></Link>
+                            <img src={corrente} />
+                            <h5>R$ 5.400,00</h5>
+                            <p>Prada Colar com Corrente</p>
+                            <Link to="/Comprar"><button>Comprar</button></Link>
                             
                         </li>
                         <li>
                             
-                                <img src={carteira} />
-                                <h5>R$ 2400.000,00</h5>
-                                <p>Gucci Carteira Couro de Carneiro</p>
-                                <Link to="/Comprar"><button>Comprar</button></Link>
+                            <img src={carteira} />
+                            <h5>R$ 2400.000,00</h5>
+                            <p>Gucci Carteira Couro de Carneiro</p>
+                            <Link to="/Comprar"><button>Comprar</button></Link>
                             
                         </li>
                         <li>
                             
-                                <img src={cinto} />
-                                <h5>R$ 1.391,00</h5>
-                                <p>Off-White Cinto Logo Industrial Feminino</p>
-                                <Link to="/Comprar"><button>Comprar</button></Link>
+                            <img src={cinto} />
+                            <h5>R$ 1.391,00</h5>
+                            <p>Off-White Cinto Logo Industrial Feminino</p>
+                            <Link to="/Comprar"><button>Comprar</button></Link>
                             
                         </li>
                         <li>
                             
-                                <img src={eyewear} />
-                                <h5>R$ 23.550,00</h5>
-                                <p>Gucci Eyewear Óculos com Tachas</p>
-                                <Link to="/Comprar"><button>Comprar</button></Link>
+                            <img src={eyewear} />
+                            <h5>R$ 23.550,00</h5>
+                            <p>Gucci Eyewear Óculos com Tachas</p>
+                            <Link to="/Comprar"><button>Comprar</button></Link>
                             
                         </li>
                         <li>
                             
-                                <img src={hardy} />
-                                <h5>R$ 23.219,00.</h5>
-                                <p>John Hardy Pulseira Modern Chain de Prata com Ouro 18K</p>
-                                <Link to="/Comprar"><button>Comprar</button></Link>
+                            <img src={hardy} />
+                            <h5>R$ 23.219,00.</h5>
+                            <p>John Hardy Pulseira Modern Chain de Prata com Ouro 18K</p>
+                            <Link to="/Comprar"><button>Comprar</button></Link>
                             
                         </li>
                     </ul>
